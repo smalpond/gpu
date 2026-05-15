52ad8504c04190ab3a3a504eb20b5b2c3905e45d
smalpond <283493512+smalpond@users.noreply.github.com>
2026-05-15 17:02:36 +0800

    Add files via upload

---
+++ 生成8A+地址.bat
@@ -0,0 +1,4 @@
+@echo off 
+echo [1;32m ¡¾Éú³É Î²ºÅ8Î» ÏàÍ¬×Ö·ûµÄÇ®°üµØÖ· - °üº¬9A 10A¡¿ [0m
+gpu.exe --lianhao address.txt  --hou 8 --quit 1
+pause
---
+++ 生成9A+地址.bat
@@ -0,0 +1,4 @@
+@echo off 
+echo [1;32m ¡¾Éú³É Î²ºÅ9Î» ÏàÍ¬×Ö·ûµÄÇ®°üµØÖ· - °üº¬9A 10A¡¿ [0m
+gpu.exe --lianhao address.txt  --hou 9 --quit 1
+pause
